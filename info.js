4d069b9913d89265c3d9c74b1c4715e24a67deae
discombobulateme <paloma.oliveira@saucelabs.com>
2022-09-07 23:44:11 +0200

    Create info.js

---
+++ info.js
@@ -0,0 +1,14 @@
+exports.cards = [
+  {
+    number: 0x00,
+    sticker_id: 'CAACAgEAAxkBAAIGsGCtRC_60FyZJ3aoM0tE9Svs7uGaAAKIAgACFYNwRSWjCrXoIPT-HwQ',
+    name: {
+      en: 'Pseudo-Random Number Generator',
+      pt: 'Gerador de Números Pseudoaleatórios',
+    },
+    message: {
+      en: 'Beginning of a journey. Every outcome is equally likely. But there is method to your madness.',
+      pt: 'Início de uma jornada. Cada possibilidade é igualmente provável. Mas existe um método para a sua loucura.',
+    },
+  },
+];
