14511e45d5f93ec410e1ac19b315c88c545dd8fd
discombobulateme <paloma.oliveira@saucelabs.com>
2022-09-08 00:11:39 +0200

    Change for simpler example

--- index.js
+++ index.js
@@ -1,68 +1,21 @@
 require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
-const { info } = require('./info.js');
 
-const TXTS = {
-  intro: {
-    en: 'Your algorithm today is:',
-    pt: 'O seu algoritmo de hoje é:',
-  },
-  outro: {
-    en: '🔮 Come back tomorrow for a new algorithm 🔮',
-    pt: '🔮 Volte amanhã para uma nova mensagem 🔮',
-  },
+const telegram = require('node-telegram-bot-api');
+
+const bot = new telegram(process.env.TELEGRAM_TOKEN);
+
+const getRandomTip = async () => {
+  var data = require('./info.json');
+  var obj = JSON.parse(JSON.stringify(data));
+  var randomNumber = Math.floor(Math.random() * 68);
+  return `Tip ${randomNumber} : ${obj.info[randomNumber].name}
+${obj.info[randomNumber].text}`;
 };
 
-function dayOfYear() {
-  const now = new Date();
-  const start = new Date(now.getFullYear(), 0, 0);
-  const diff = now - start;
-  const oneDay = 1000 * 60 * 60 * 24;
-  const day = Math.floor(diff / oneDay);
-  return day;
-}
+const main = async () => {
+  const randomTip = await getRandomTip();
+  bot.sendMessage(process.env.TELEGRAM_TOKEN, randomTip);
+  console.log(randomTip);
+};
 
-function dailyRandom(mSeed) {
-  const x = Math.sin(mSeed) * 1e4;
-  return x - Math.floor(x);
-}
-
-const bot = new TelegramBot({
-  token: process.env.TELEGRAM_TOKEN,
-  polling: {
-    interval: 1000,
-    timeout: 0,
-    limit: 100,
-    retryTimeout: 5000,
-  },
-  allowedUpdates: ['text'],
-  usePlugins: [],
-});
-
-bot.on('sticker', (msg) => {
-  console.log(msg.sticker);
-  return msg.reply.text(msg.sticker.file_id);
-});
-
-function botReply(bot, msg, lang) {
-  const mIndex = Math.floor(info.length * dailyRandom(msg.from.id + dayOfYear()));
-  const mCard = info[mIndex];
-
-  msg.reply.text(`${TXTS.intro[lang]}\n${mCard.name[lang]}`).then(() => {
-    bot.sendSticker(msg.chat.id, mCard.sticker_id).then(() => {
-      msg.reply.text(`${mCard.message[lang]}`).then(() => {
-        msg.reply.text(`${TXTS.outro[lang]}`);
-      });
-    });
-  });
-}
-
-bot.on(['/draw'], (msg) => {
-  botReply(bot, msg, 'en');
-});
-
-bot.on(['/carta'], (msg) => {
-  botReply(bot, msg, 'pt');
-});
-
-bot.start();
+main();
